b902d128aa136acafb529d16e6154f92c1a7d3cb
Tomek Marciniak <tmarciniakm@gmail.com>
2023-07-10 09:47:09 +0200

    feat(init): init

--- packages/heat/src/components/Box/index.stories.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Box } from './index'
-
-export const Basic = () => <Box>Test</Box>
-export const CustomStyles = () => (
-  <Box css={{ border: '1px red solid' }}>Test</Box>
-)
